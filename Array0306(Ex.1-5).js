a555ced5189d11a551b5815420a0e9ea0fff004f
Hanna Chernihina <40338698+HannaChernihina@users.noreply.github.com>
2021-06-06 19:46:23 +0300

    Update Array0306(Ex.1-5).js

--- Array0306(Ex.1-5).js
+++ Array0306(Ex.1-5).js
@@ -37,7 +37,11 @@ console.log(copyArray([1, 5, 4, 0]))
 // [7, 9, 0]
 // [7, 9, 0, -2]
 // []
+function first(arr, n){
+    return arr.slice(0, n)
+}
 
+console.log(first([7, 9, 0, -2],3));
 
 // _______________________________________________________________________
 // 4. Write a JavaScript function to get the last element of an array. Passing a parameter 'n' will return the last 'n' elements of the array. Go to the editor
