dbd42726ebbea25b9b0856a2bca7512f420d45cb
Hanna Chernihina <40338698+HannaChernihina@users.noreply.github.com>
2021-06-06 16:40:47 +0300

    Update objects.js

--- objects.js
+++ objects.js
@@ -1,32 +1,29 @@
-1) 
-Write function getUserInfo which receive user object + string with key for user data.
-For example;
-obj  = {fullName: "fds fds", age: 5}
-key = "age"
-return 5;
+// 1) 
+// Write function getUserInfo which receive user object + string with key for user data.
+// For example;
+// obj  = {fullName: "fds fds", age: 5}
+// key = "age"
+// return 5;
 
-
-function getUserInfo(name, year){ 
-const obj = {};
-obj.fullName = name;
-obj.age = year;
-const result = obj.age;
-return result;
+function getUserInfo(e){ 
+return user['age'];
 }
-getUserInfo('Hanna', 25);
 
-_______________________________________________________________________________________
-2) Write function checkPropInObj which receive object and property and return true/false
-For example;
-obj  = {fullName: "fds fds", age: 5}
-property = "age"
-return true/false
-return 5;
+const user = {
+               age: 25,
+              };
+getUserInfo(user);
+
+// _______________________________________________________________________________________
+// 2) Write function checkPropInObj which receive object and property and return true/false
+// For example;
+// obj  = {fullName: "fds fds", age: 5}
+// property = "age"
+// return true/false
 
 function checkPropInObj(e){ 
-   key = 'age';
- const x = key in e;
-    return x;
+ return 'age' in e;
 }
+
 obj  = {fullName: "fds fds", age: 5};
 checkPropInObj(obj);
