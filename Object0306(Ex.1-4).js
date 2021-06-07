011045236fc6a81f4f091a4003da3188df8ee70d
Hanna Chernihina <40338698+HannaChernihina@users.noreply.github.com>
2021-06-07 15:02:36 +0300

    Create Object0306(Ex.1-4).js

---
+++ Object0306(Ex.1-4).js
@@ -0,0 +1,49 @@
+// 1. Write a JavaScript program to list the properties of a JavaScript object. Go to the editor
+// Sample object:
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+// Sample Output: name,sclass,rollno
+
+
+
+// ___________________________________________________________________________________________________
+// 2. Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property. Go to the editor
+// Sample object:
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+
+
+
+
+// ___________________________________________________________________________________________________
+// 3. Write a JavaScript program to get the length of a JavaScript object. Go to the editor
+// Sample object :
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+
+
+// ___________________________________________________________________________________________________
+// 4. Write a JavaScript program to display the reading status (i.e. display book name, author name and reading status) of the following books. Go to the editor
+
+// var library = [ 
+//    {
+//        author: 'Bill Gates',
+//        title: 'The Road Ahead',
+//        readingStatus: true
+//    },
+//    {
+//        author: 'Steve Jobs',
+//        title: 'Walter Isaacson',
+//        readingStatus: true
+//    },
+//    {
+//        author: 'Suzanne Collins',
+//        title:  'Mockingjay: The Final Book of The Hunger Games', 
+//        readingStatus: false
+//    }];
