f8c9a18417dd2e1778589b8767084e627b2b00f6
Hanna Chernihina <40338698+HannaChernihina@users.noreply.github.com>
2021-06-29 01:23:46 +0300

    Update arrayUsers.js

--- arrayUsers.js
+++ arrayUsers.js
@@ -63,15 +63,17 @@ let users = [{"id":1,"firstName":"Harriette","userName":"hgordongiles0"},
 
 function addAge(arr){
     return arr.map((item) => {
+    
         if(item.userName.length > 7){
             item.tooOld = true;
-        } else if (item.userName.length < 7){
+
+        } else if (item.userName.length <= 7){
+        
             item.tooYoung = true;
         } 
         return item;  
-     });
-
- }
+});
+}
 
 
 
