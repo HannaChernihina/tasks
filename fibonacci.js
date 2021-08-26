963e4c8531d0f92cbde1e94139de355f6ce6e40d
Hanna Chernihina <40338698+HannaChernihina@users.noreply.github.com>
2021-08-26 21:29:18 +0300

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,4 +1,20 @@
- // 1. Последовательность чисел Фибоначчи определяется формулой Fn = Fn-1 + Fn-2 
+ 
+// Write a JavaScript program to calculate the factorial of a number.
+// In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example, 5! = 5 x 4 x 3 x 2 x 1 = 120 
+
+// 2)  Write a JavaScript program to get the integers in range (x, y).
+// Example : range(2, 9)
+// Expected Output : [3, 4, 5, 6, 7, 8]
+
+// 3) Write a JavaScript program to get the first n Fibonacci numbers.
+// Note : The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, . . . Each subsequent number is the sum of the previous two. 
+
+
+
+
+
+
+// 1. Последовательность чисел Фибоначчи определяется формулой Fn = Fn-1 + Fn-2 
 function fibonacci(n){
 if(n === 0)
     return 0;
