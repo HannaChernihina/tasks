4b48a68565dc1df5f534eaf9887e550a5f1a0ea1
Hanna Chernihina <40338698+HannaChernihina@users.noreply.github.com>
2021-09-27 13:00:36 +0300

    Update allLesson.js

--- allLesson.js
+++ allLesson.js
@@ -1,96 +1,96 @@
 
-1. Create a for loop that iterates up to 100 while outputting "fizz" at multiples of 3, "buzz" at multiples of 5 and "fizzbuzz" at multiples of 3 and 5
+// 1. Create a for loop that iterates up to 100 while outputting "fizz" at multiples of 3, "buzz" at multiples of 5 and "fizzbuzz" at multiples of 3 and 5
 
-1. Write a JavaScript function to check whether an `input` is an array or not. Go to the editor
-Test Data :
-console.log(is_array('w3resource'));
-console.log(is_array([1, 2, 4, 0]));
-false
-true
-Click me to see the solution
-
-2. Write a JavaScript function to clone an array. Go to the editor
-Test Data :
-console.log(array_Clone([1, 2, 4, 0]));
-console.log(array_Clone([1, 2, [4, 0]]));
-[1, 2, 4, 0]
-[1, 2, [4, 0]]
-Click me to see the solution
-
-3. Write a JavaScript function to get the first element of an array. Passing a parameter 'n' will return the first 'n' elements of the array. Go to the editor
-Test Data :
-console.log(first([7, 9, 0, -2]));
-console.log(first([],3));
-console.log(first([7, 9, 0, -2],3));
-console.log(first([7, 9, 0, -2],6));
-console.log(first([7, 9, 0, -2],-3));
-Expected Output :
-7
-[]
-[7, 9, 0]
-[7, 9, 0, -2]
-[]
-Click me to see the solution
-
-4. Write a JavaScript function to get the last element of an array. Passing a parameter 'n' will return the last 'n' elements of the array. Go to the editor
-Test Data :
-console.log(last([7, 9, 0, -2]));
-console.log(last([7, 9, 0, -2],3));
-console.log(last([7, 9, 0, -2],6));
-Expected Output :
--2
-[9, 0, -2]
-[7, 9, 0, -2]
-Click me to see the solution
-
-5. Write a simple JavaScript program to join all elements of the following array into a string. Go to the editor
-Sample array : myColor = ["Red", "Green", "White", "Black"];
-Expected Output :
-"Red,Green,White,Black"
-"Red,Green,White,Black"
-"Red+Green+White+Black"
-Click me to see the solution
+// 1. Write a JavaScript function to check whether an `input` is an array or not. Go to the editor
+// Test Data :
+// console.log(is_array('w3resource'));
+// console.log(is_array([1, 2, 4, 0]));
+// false
+// true
 
 
-1. Write a JavaScript program to list the properties of a JavaScript object. Go to the editor
-Sample object:
-var student = {
-name : "David Rayy",
-sclass : "VI",
-rollno : 12 };
-Sample Output: name,sclass,rollno
-Click me to see the solution
+// 2. Write a JavaScript function to clone an array. Go to the editor
+// Test Data :
+// console.log(array_Clone([1, 2, 4, 0]));
+// console.log(array_Clone([1, 2, [4, 0]]));
+// [1, 2, 4, 0]
+// [1, 2, [4, 0]]
 
-2. Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property. Go to the editor
-Sample object:
-var student = {
-name : "David Rayy",
-sclass : "VI",
-rollno : 12 };
-Click me to see the solution
 
-3. Write a JavaScript program to get the length of a JavaScript object. Go to the editor
-Sample object :
-var student = {
-name : "David Rayy",
-sclass : "VI",
-rollno : 12 };
-Click me to see the solution
+// 3. Write a JavaScript function to get the first element of an array. Passing a parameter 'n' will return the first 'n' elements of the array. Go to the editor
+// Test Data :
+// console.log(first([7, 9, 0, -2]));
+// console.log(first([],3));
+// console.log(first([7, 9, 0, -2],3));
+// console.log(first([7, 9, 0, -2],6));
+// console.log(first([7, 9, 0, -2],-3));
+// Expected Output :
+// 7
+// []
+// [7, 9, 0]
+// [7, 9, 0, -2]
+// []
 
-4. Write a JavaScript program to display the reading status (i.e. display book name, author name and reading status) of the following books. Go to the editor
-var library = [ 
-   {
-       author: 'Bill Gates',
-       title: 'The Road Ahead',
-       readingStatus: true
-   },
-   {
-       author: 'Steve Jobs',
-       title: 'Walter Isaacson',
-       readingStatus: true
-   },
-   {
-       author: 'Suzanne Collins',
-       title:  'Mockingjay: The Final Book of The Hunger Games', 
-       readingStatus: false
-   }];
+
+// 4. Write a JavaScript function to get the last element of an array. Passing a parameter 'n' will return the last 'n' elements of the array. Go to the editor
+// Test Data :
+// console.log(last([7, 9, 0, -2]));
+// console.log(last([7, 9, 0, -2],3));
+// console.log(last([7, 9, 0, -2],6));
+// Expected Output :
+// -2
+// [9, 0, -2]
+// [7, 9, 0, -2]
+
+
+// 5. Write a simple JavaScript program to join all elements of the following array into a string. Go to the editor
+// Sample array : myColor = ["Red", "Green", "White", "Black"];
+// Expected Output :
+// "Red,Green,White,Black"
+// "Red,Green,White,Black"
+// "Red+Green+White+Black"
+
+
+
+// 1. Write a JavaScript program to list the properties of a JavaScript object. Go to the editor
+// Sample object:
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+// Sample Output: name,sclass,rollno
+
+
+// 2. Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property. Go to the editor
+// Sample object:
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+
+
+// 3. Write a JavaScript program to get the length of a JavaScript object. Go to the editor
+// Sample object :
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+
+
+// 4. Write a JavaScript program to display the reading status (i.e. display book name, author name and reading status) of the following books. Go to the editor
+// var library = [ 
+//    {
+//        author: 'Bill Gates',
+//        title: 'The Road Ahead',
+//        readingStatus: true
+//    },
+//    {
+//        author: 'Steve Jobs',
+//        title: 'Walter Isaacson',
+//        readingStatus: true
+//    },
+//    {
+//        author: 'Suzanne Collins',
+//        title:  'Mockingjay: The Final Book of The Hunger Games', 
+//        readingStatus: false
+//    }];
