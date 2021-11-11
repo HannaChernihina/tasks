ead11d7bf4d3ae923d084ed1fa88f549080beb84
Hanna Chernihina <40338698+HannaChernihina@users.noreply.github.com>
2021-11-11 11:42:10 +0300

    Update getter-setter--definesProperties.js

--- getter-setter--definesProperties.js
+++ getter-setter--definesProperties.js
@@ -1,1 +1,43 @@
+// 1.
+// function User(firstName, birthday) {
+// this.firstName = firstName
+// this.birthday = birthday
+// }
+// create get & set for "age" property
+// get age -calculate duration time from birthday in years
+// set age - set age from today - number of ages
+// set -> Date.now() - age (from setter
+// get  {
+// Date.now - birthday 
+// }
 
+function User(firstName, birthday) {
+  this.firstName = firstName;
+  this.birthday = birthday;
+
+  Object.defineProperties(this, {
+      age: {
+          enumerable: true,
+          configurable: true,
+
+          get(){
+
+             let yearMs = Date.now() - Date.parse(this.birthday);
+             let year = new Date(yearMs);
+             let result =  Math.abs( year.getUTCFullYear() - 1970 )
+
+             return ${this.firstName} - ${result};
+
+          },
+
+          set(age){  
+
+          let data = new Date();
+          let yearNow = data.getFullYear() - age
+          data.setFullYear(yearNow)
+          this.birthday = data;
+          }
+     }
+  })
+}
+// _______________________________________________________________________________________
