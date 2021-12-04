2f053ad360e9e5f077218a724b6e3fbcd0a0f96e
Hanna Chernihina <40338698+HannaChernihina@users.noreply.github.com>
2021-12-04 14:00:06 +0200

    Update function for filter.js

--- function for filter.js
+++ function for filter.js
@@ -1,7 +1,11 @@
-// 1. we have a build-in method arr.filter(f) for arrays. 
-// make a set of ready to use filter
+// 1. we have a build-in method arr.filter(f) for arrays. It filters all elements through the function f.
+// If it returns true, then that elements is returned in the resulting array.
+
+// make a set of ready to use filter.
 // inBetween(a, b)  betqeen a and b or equel to them (inclusively включительно )
 // inArray([...])  in the given array.
+// arr.filter(inBetween(a, b)) - selects only values between 3 and 6.
+// arr.filter(inArray([1,2,10])) select only elements matching with one of the members of [1, 2, 3]
 
 
 function inBetween(a, b){
