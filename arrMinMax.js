5273148fcc551466928976c9aace82de6425a7e9
Hanna Chernihina <40338698+HannaChernihina@users.noreply.github.com>
2021-12-04 13:22:33 +0200

    Create arrMinMax.js

---
+++ arrMinMax.js
@@ -0,0 +1,19 @@
+// 1. use ES6 spread operator to find the minimum value of these mumbers and log it to the console.
+// And let`s log the list of numbers themselves, to make it clearer. So the output should be, foe example:
+// let arr = [18, 5, 9, 8] //5
+
+function minMax(arr){
+return Math.min.apply(this, arr)
+}
+
+
+function min(arr){
+   return arr.reduce((a,b) => {
+    return  (a < b) ? a : b;
+})
+}
+
+
+function minMax(arr){
+return Math.min(...arr)
+}
